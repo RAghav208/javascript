e8c70effd645db07bd3eb95e43e3080a047cb85a
Raghav Kejriwal <37446807+RAghav208@users.noreply.github.com>
2025-02-20 14:07:58 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,9 @@ const accountId = 1254545
 let accountEmail = "raghav@gmail.com"
 var accountPassowrd = "12345"
 accountCity = "Delhi"
+let accountState;
 
 // accountId =5
 
 console.log(accountId);
+console.table([accountId, accountEmail, accountPassowrd, accountCity, accountState]);
