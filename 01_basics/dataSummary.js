4cbd56a8dc51da715d6bd3aa39cbf2ff2ef231de
Raghav Kejriwal <37446807+RAghav208@users.noreply.github.com>
2025-02-21 11:10:45 +0000

    datatypes summary

---
+++ 01_basics/dataSummary.js
@@ -0,0 +1,18 @@
+//primitive
+// 7 tyoes: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100 
+
+//Refrence Type(Non Primitive)
+//Array, Objects, Functions
+
+const heros = ["saktiman", "naagraj", "doga"]
+let myObj= {
+    name: "raghav",
+    age: 22,
+}
+const myFunction= function(){
+    console.log("Hello World");
+}
+
+    
