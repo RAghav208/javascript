e7777d494a0ad2ec10d233d566a76e9d9fd1d2d6
Raghav Kejriwal <37446807+RAghav208@users.noreply.github.com>
2025-03-08 15:27:43 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name= "Raghav";
+const repoCount= 4
+// console.log(name + repoCount+ "value");
+
+console.log(`Hello my name is ${name} any my repo count is ${repoCount}`)
+
+
+const gameName= new String('raghav-hc')
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('h'));
+
+console.log(gameName. );
+
+
+
+
